f5e0d84d2d3bb33e0bec3475f3c7b2f04ae8eaa1
Wellyngtron <128184659+Wellyngtron@users.noreply.github.com>
2026-05-06 15:31:01 +0000

    Adicionado repositorio Reserva

---
+++ ESW_3_JAVA_SPRINGBOOT/demo/src/main/java/com/example/demo/Repository/Reservarepositorio.java
@@ -0,0 +1,13 @@
+/*  Autor: Wellyngton Gabriel Randolfo Ramos
+    repositorio: reserva
+*/
+package com.example.demo.Repository;
+
+import java.util.List;
+import com.example.demo.Entities.Reserva;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Reservarepositorio extends JpaRepository<Reserva, Long> {
+    List<Reserva> findByStatusreserva(String statusreserva);
+}
