1e415af2607cf6609e8a05d789c97db6dc27ef60
Kiry <caiokirydev15@gmail.com>
2026-05-06 00:01:02 -0300

    ClienterequestDTO/1.0

--- ESW_3_JAVA_SPRINGBOOT/demo/src/main/java/com/example/demo/DTO/request/ClienterequestDTO.java
+++ ESW_3_JAVA_SPRINGBOOT/demo/src/main/java/com/example/demo/DTO/request/ClienterequestDTO.java
@@ -1,5 +1,22 @@
-package com.example.demo.DTO;
+package com.example.demo.DTO.request;
 
-@Data
-@NoArgsConstructor
-public class ClienteDTO {}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record ClienterequestDTO(
+        @NotBlank(message = "Nome não pode vir vazio")
+        String nome,
+
+        @NotBlank(message = "Telefone não pode vir vazio")
+        String telefone,
+
+        @NotBlank(message = "Email não pode vir vazio")
+        @Email
+        String email,
+
+        String endereco,
+
+        @NotBlank(message = "Documento não pode vir vazio")
+        int documento
+) {
+}
