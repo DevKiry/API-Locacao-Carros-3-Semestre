a4dcb855321c39be3543b3b20e063c8c756c5c04
Wellyngtron <128184659+Wellyngtron@users.noreply.github.com>
2026-05-06 19:51:42 -0300

    Adicionado Import JpaRepository

--- ESW_3_JAVA_SPRINGBOOT/demo/src/main/java/com/example/demo/Repository/ReservaRepository.java
+++ ESW_3_JAVA_SPRINGBOOT/demo/src/main/java/com/example/demo/Repository/ReservaRepository.java
@@ -5,6 +5,7 @@ package com.example.demo.Repository;
 
 import java.util.List;
 import com.example.demo.Entities.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
