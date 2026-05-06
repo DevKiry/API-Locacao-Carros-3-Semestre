d264cb709b026785196913a043f8e7757f75c575
GustavoSh5 <gustavoshinozaki1@gmail.com>
2026-05-06 01:04:39 -0300

    ManutencaoDTO

---
+++ ESW_3_JAVA_SPRINGBOOT/demo/src/main/java/com/example/demo/DTO/request/ManutencaoDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.DTO;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ManutencaoDTO {
+
+    private Long carroId;
+
+    private LocalDateTime dataInicio;
+
+    private LocalDateTime dataFim;
+
+    @NotBlank(message = "A descrição é obrigatória")
+    private String descricao;
+
+    @NotBlank(message = "O Status é obrigatório")
+    private String status;
+
+}
