70787f1aa8a4a74e369198c84cc61a0513e5ebcc
João Victor <joaovictorrantinsilverio2007@gmail.com>
2026-05-06 19:58:37 -0300

    PagamentoRequestDTO adicionado

---
+++ ESW_3_JAVA_SPRINGBOOT/demo/src/main/java/com/example/demo/DTO/request/PagamentoRequestDTO.java
@@ -0,0 +1,22 @@
+package com.example.demo.DTO.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record PagamentoRequestDTO (
+    Long id,
+
+    @NotNull(message = "O valor é obrigatório")
+    BigDecimal valor,
+
+    LocalDateTime dataPagamento,
+
+    @NotBlank(message = "O Status é obrigatório")
+    String status,
+
+    @NotBlank(message = "A forma de pagamento deve ser obrigatória")
+    String formaPagamento
+){}
