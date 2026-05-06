f65fee126d39782bfb7eadbe53266be26dbf4aea
GustavoSh5 <gustavoshinozaki1@gmail.com>
2026-05-06 01:00:29 -0300

    Entidade Manutenção

---
+++ ESW_3_JAVA_SPRINGBOOT/demo/src/main/java/com/example/demo/Entities/Manutencao.java
@@ -0,0 +1,41 @@
+package com.example.demo.Entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Manutencao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime dataInicio;
+
+    private LocalDateTime dataFim;
+
+    @Column(nullable = false)
+    private String descricao;
+    
+    @Column(nullable = false)
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "carro_id")
+    private Carro carro;
+}
+
