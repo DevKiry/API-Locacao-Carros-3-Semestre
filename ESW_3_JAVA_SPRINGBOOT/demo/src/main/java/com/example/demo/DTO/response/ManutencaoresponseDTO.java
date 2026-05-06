abfac7187285bb941d51d8beb4c6159884f817b8
GustavoSh5 <gustavoshinozaki1@gmail.com>
2026-05-06 01:15:08 -0300

    ManutencaoresponseDTO

---
+++ ESW_3_JAVA_SPRINGBOOT/demo/src/main/java/com/example/demo/DTO/response/ManutencaoresponseDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.DTO.response;
+
+import java.time.LocalDateTime;
+
+public record ManutencaoresponseDTO(
+    
+    Long id,
+    Long carroId,
+    LocalDateTime dataInicio,
+    LocalDateTime dataFim,
+    String descricao,
+    String status
+) {}
